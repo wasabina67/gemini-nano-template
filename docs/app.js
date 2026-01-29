7edf0e4b808cd38a44a8fc97d324900d437c1590
wasabina67 <wasabina67@gmail.com>
2026-01-29 19:11:18 +0900

    update

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,9 @@
+const LANGUAGE = 'ja';
+const LANGUAGE_OPTIONS = {
+  expectedInputs: [{ type: 'text', languages: [LANGUAGE] }],
+  expectedOutputs: [{ type: 'text', languages: [LANGUAGE] }],
+};
 
+async function init() {}
+
+init();
